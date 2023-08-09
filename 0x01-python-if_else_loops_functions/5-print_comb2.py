3683c543e57ccbba528de884272dd36605271e69
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-10 00:48:57 +0200

    check 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+for a in range(100):
+    print("{:d}".format(a).rjust(2, "0"),
+          end="\n" af a == 99 else ", ")
