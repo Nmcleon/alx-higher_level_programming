3ac1c1757ba1414c7dc555b0f03b9c21f858c36c
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-15 23:50:04 +0200

    Module containing the "Square" class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""
+Module containing the "Square" class
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
