75aa0aa5a42bece55d9748a6effafe27d9a668fe
Nmcleon <nmcleon10@gmail.com>
2023-09-15 16:19:11 -0700

    code style err

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """A representation of a rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle"""
         super().__init__(id)
