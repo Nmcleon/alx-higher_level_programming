f2f19d206401ba062ec7d40eae4eb3e246375299
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-17 21:56:54 +0200

    Update __init__.py

--- 0x0C-python-almost_a_circle/tests/__init__.py
+++ 0x0C-python-almost_a_circle/tests/__init__.py
@@ -1,1 +1,16 @@
+@property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """informal string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
 
