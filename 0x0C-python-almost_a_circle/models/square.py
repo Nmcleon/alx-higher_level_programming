29aefabc94fac9c76ae067c7acca08f71da6bf6f
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-18 15:15:02 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,6 +24,10 @@ def size(self):
 @size.setter
 def size(self, value):
     """setter for size"""
+    if type(value) is not int:
+        raise TypeError("width must be an integer")
+    if value <= 0:
+        raise ValueError("width must be > 0")
     self.width = value
     self.height = value
 
