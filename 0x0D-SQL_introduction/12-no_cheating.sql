685b4220a3399cc73de73da2247bb27d7dc714f6
Nmcleon <nmcleon10@gmail.com>
2023-10-17 14:59:16 -0700

    update 12-.

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table second_table.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-ORDER BY `score` DESC;
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
