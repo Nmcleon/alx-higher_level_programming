66ea1e9e02d069a8c6fa1284fffabdd7d7decf43
Nmcleon <nmcleon10@gmail.com>
2023-11-16 11:15:03 -0800

    semistandatd err

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		if (w > 0 && h > 0){
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print (c = 'X') {
-		for (let i = 0; i < this.height; i++) {
-			console.log(c.repeat(this.width));
-		}
-	}
+  print (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Rectangle;
