e9ac4d5e95ba42b64c44623d898d20548cab259a
Nmcleon <nmcleon10@gmail.com>
2023-11-19 10:21:56 -0800

    fix pep8

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -7,9 +7,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-    
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(256), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref="state")
+    cities = relationship(
+            "City",
+            cascade="all, delete-orphan",
+            backref="state")
