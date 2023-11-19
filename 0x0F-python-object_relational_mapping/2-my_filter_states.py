26ef42f57d4271e207e7c57333500dfffa9d75ea
Nmcleon <nmcleon10@gmail.com>
2023-11-19 14:39:55 +0200

    test 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,32 +5,27 @@ Script that displays all values in the states table of hbtn_0e_0_usa where name 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
         exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            passwd=password,
-            db=database
-        )
-        cur = db.cursor()
-        num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
-                               '{}' ORDER BY states.id;".format(state_name))
-        
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-
-        cur.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    state_name = args[4]
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=data,
+        port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
+                           '{}' ORDER BY states.id;".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
