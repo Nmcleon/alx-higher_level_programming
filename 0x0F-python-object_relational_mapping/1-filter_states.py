acadd82bcdc687819bd690233ad83fc71ab0d8a7
Nmcleon <nmcleon10@gmail.com>
2023-11-19 03:26:41 -0800

    complete 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,37 @@
+#!/usr/bin/python3
+"""
+Script lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                passwd=password,
+                db=database
+                )
+        cur = db.cursor()
+        cur.execute(
+                "SELECT * FROM states "
+                "WHERE name LIKE BINARY 'N%' "
+                "ORDER BY id"
+                )
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
