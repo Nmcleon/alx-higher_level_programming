d5db41fc5eb223c99b6582e2759cb1ab5d4946b6
N <51336368+Nmcleon@users.noreply.github.com>
2023-12-02 18:13:13 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """POST an email #1"""
 
+if __name__ == "__main__":
 from requests import post
 from sys import argv
 
-if __name__ == "__main__":
     html = post(argv[1]
     data={'email': argv[2]})
     print(html.text)
