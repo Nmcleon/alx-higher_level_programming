8e1bd2cff845e3c6c89feda4fd36cc58cae21aa0
Nmcleon <nmcleon10@gmail.com>
2023-12-02 08:29:04 -0800

    fix pycodestle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@
     if __name__ == "__main__":
       import urllib.request
       import sys
-        
+
      with urllib.request.urlopen(sys.argv[1]) as response:
         head = response.headers.get('X-Request-Id')
         print(head)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,6 @@
 if __name__ == "__main__":
     from requests import post
     from sys import argv
-    
+
     html = post(argv[1], data={'email': argv[2]})
     print(html.text)
