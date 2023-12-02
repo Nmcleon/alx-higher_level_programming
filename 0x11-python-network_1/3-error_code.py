8747a4f0d2ed5ae2c3a667bc3ab9edfebfe008e3
Nmcleon <nmcleon10@gmail.com>
2023-12-02 07:00:39 -0800

    update 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Error code #0 """
 
-from urllib import request, parse
+form urllib.request import urlopen
+from urllib.err import HTTPError
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-    value = {"email": argv[2]}
-    header = parse.urlencode(value).encode()
-    with request.urlopen(request.Request(url, header)) as response:
-        print(response.read().decode("utf-8"))
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as error:
+        print(f'Error code: {error.status}')
