f18c7bc7844571a51ed44d970c50c214e4ec46e6
Nmcleon <nmcleon10@gmail.com>
2023-12-02 06:34:43 -0800

    complete 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" What's my status? """
+
+from urllib.request import urlopen
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as f:
+        body = f.read()
+        print('Body response:')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t- utf8 content: {body.decode("utf-8")}')
