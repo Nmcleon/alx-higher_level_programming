2985f0c4c777ed6aaffd163032211e882e940207
N <51336368+Nmcleon@users.noreply.github.com>
2023-12-02 18:17:41 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Error code #0 """
 
+if __name__ == "__main__":
 from urllib.request import urlopen
 from urllib.err import HTTPError
 from sys import argv
-
-if __name__ == "__main__":
+    
     try:
         with urlopen(argv[1]) as response:
             print(response.read().decode("utf-8"))
