e76e78747128c8f25c606bf5cc37fe93406f81e2
Nmcleon <nmcleon10@gmail.com>
2023-12-02 07:37:10 -0800

    fix pep8

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status").text
     print("Body response:")
     print(f'\t- type: {type(response)}')
-    print(f'\t- content: {response}')                    
+    print(f'\t- content: {response}')
